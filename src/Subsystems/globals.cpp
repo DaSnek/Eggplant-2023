f4f3fade952997d60912a75c00c04a47f3b53bc3
Jiayi Zhang <jz5883@pleasantonusd.net>
2023-11-17 18:39:56 -0800

    removed triport expander

--- src/Subsystems/globals.cpp
+++ src/Subsystems/globals.cpp
@@ -3,5 +3,5 @@
 
 okapi::Controller controller;
 
-pneumaticSys wings(EXPANDER_PORT, WINGS_ADI, WINGS_ACTUATE_O, WINGS_ACTUATE_I);
-pneumaticSys t_bar(EXPANDER_PORT, T_BAR_ADI, T_BAR_ACTUATE_U, T_BAR_ACTUATE_D);
+pneumaticSys wings(WINGS_ADI, WINGS_ACTUATE_O, WINGS_ACTUATE_I);
+pneumaticSys t_bar(T_BAR_ADI, T_BAR_ACTUATE_U, T_BAR_ACTUATE_D);
